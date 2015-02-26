6c6eaf48445f673114c4ed9380d2c26a558fd075
Afton Hencky <ahencky@gmail.com>
2015-02-26 15:36:05 -0800

    user controller file set up

---
+++ app/controllers/user.rb
@@ -0,0 +1,44 @@
+get '/' do
+
+
+
+end
+
+get '/user/:id' do
+
+
+
+
+end
+
+get '/login' do
+
+
+
+
+
+end
+
+post '/login' do
+
+
+
+
+end
+
+get '/register' do
+
+
+
+
+
+end
+
+post '/register' do
+
+
+
+
+
+end
+
