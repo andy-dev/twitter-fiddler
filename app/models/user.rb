3f88eb5117c458cc45c36fa4cd8839ca600de848
Afton Hencky <ahencky@gmail.com>
2015-02-26 15:24:26 -0800

    db ready

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_many :follow_relationships, :class_name => "Relationship", :foreign_key => "followed_id"
+  has_many :followers, :through => :follow_relationships, :source => :following
+
+  has_many :followed_relationships, :class_name => "Relationship", :foreign_key => "follower_id"
+  has_many :followings, :through => :followed_relationships, :source => :follower
+end
